4435cedf4e5a8cae8ca359a05607ebdcda855f19
91Seconds <king.ciaran72@gmail.com>
2016-07-26 15:16:55 +1200

    Mavenize this shit

---
+++ RoboticArrrmAPI/src/main/java/net/tangentmc/AngleTuple.java
@@ -0,0 +1,17 @@
+package net.tangentmc;
+
+public class AngleTuple {
+    double theta1,theta2;
+
+    public AngleTuple(double theta1, double theta2) {
+        this.theta1=theta1;
+        this.theta2=theta2;
+    }
+
+    public double getTheta1() {
+        return theta1;
+    }
+    public double getTheta2() {
+        return theta2;
+    }
+}
--- RoboticArrrmAPI/src/main/java/net/tangentmc/Utils.java
+++ RoboticArrrmAPI/src/main/java/net/tangentmc/Utils.java
@@ -59,24 +59,4 @@ public class Utils {
         return Co1+proportion*(Co2-Co1);
 
     }
-
-    @ToString
-//    @AllArgsConstructor
-    @Getter
-    public static class AngleTuple {
-        double theta1,theta2;
-
-        public AngleTuple(double theta1, double theta2) {
-            this.theta1=theta1;
-            this.theta2=theta2;
-        }
-
-        public double getTheta1() {
-            return theta1;
-        }
-        public double getTheta2() {
-            return theta2;
-        }
-    }
-
 }
