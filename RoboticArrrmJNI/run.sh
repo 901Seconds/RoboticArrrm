0b455876e91de40dbbe4620747d00649f2859647
sanjay900 <sanjay.govind9@gmail.com>
2016-09-09 10:32:48 +1200

    Move scripts to root, remove unnecessary library declartions in script since thats automated now.

--- RoboticArrrmJNI/run.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-cd target
-sudo java -Djava.library.path=nar/RoboticArrrmJNI-1.0-SNAPSHOT-arm-Linux-gpp-jni/lib/arm-Linux-gpp/jni/ -jar RoboticArrrmJNI-1.0-SNAPSHOT.jar
-cd ..
-
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd RoboticArrrmLauncher/target
+sudo java -jar RoboticArrrmJNI-1.0-SNAPSHOT.jar
+cd ..
+
