c45d68524eebaa18f7eb4665345a3258fb7c71e7
sanjay900 <sanjay.govind9@gmail.com>
2016-09-08 14:38:56 +1200

    test

--- RoboticArrrmJNI/src/main/c++/arm2.cpp
+++ RoboticArrrmJNI/src/main/c++/arm2.cpp
@@ -5,27 +5,6 @@
 #include <sys/time.h>
 #include <math.h>
 
-extern "C" int init(int);
-
-// motor 
-extern "C" int set_motor(int m, int s);
-extern "C" int Sleep(int s,int mu);
-extern "C" int set_PWM(int chan, int value);
-extern "C" int set_PWM_frequency(int chan, int freq);
-extern "C" int set_servo(int chan, int value);
-extern "C" int select_IO(int chan, int direct);
-extern "C" int write_digital(int chan,char level);
-
-//camera
-extern "C" int take_picture();
-extern "C" int open_screen_stream();
-extern "C" int close_screen_stream();
-extern "C" int take_picture();
-extern "C" int update_screen();
-extern "C" int save_picture(char fn[5]);
-extern "C" int set_pixel(int x,int y,char r,char g,char b); 
-extern "C" char get_pixel(int x,int y,char rgb);
-
 typedef struct {
    int xm1;
    int ym1;
--- RoboticArrrmJNI/src/main/c++/arm2.h
+++ RoboticArrrmJNI/src/main/c++/arm2.h
@@ -1,3 +1,25 @@
+
+
+extern "C" int init(int);
+
+// motor
+extern "C" int set_motor(int m, int s);
+extern "C" int Sleep(int s,int mu);
+extern "C" int set_PWM(int chan, int value);
+extern "C" int set_PWM_frequency(int chan, int freq);
+extern "C" int set_servo(int chan, int value);
+extern "C" int select_IO(int chan, int direct);
+extern "C" int write_digital(int chan,char level);
+
+//camera
+extern "C" int take_picture();
+extern "C" int open_screen_stream();
+extern "C" int close_screen_stream();
+extern "C" int take_picture();
+extern "C" int update_screen();
+extern "C" int save_picture(char fn[5]);
+extern "C" int set_pixel(int x,int y,char r,char g,char b);
+extern "C" char get_pixel(int x,int y,char rgb);
 void measure_angles(int);
 void draw_motors();
 void pen_up_down(int dir);
