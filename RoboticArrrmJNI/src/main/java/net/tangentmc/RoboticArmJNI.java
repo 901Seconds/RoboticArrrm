673cb00242c1d009aae2cef680b8f54cfadf8fae
sanjay900 <sanjay.govind9@gmail.com>
2016-09-09 15:35:15 +1200

    Oh.

--- RoboticArrrmJNI/src/main/java/net/tangentmc/RoboticArmJNI.java
+++ RoboticArrrmJNI/src/main/java/net/tangentmc/RoboticArmJNI.java
@@ -15,21 +15,6 @@ public class RoboticArmJNI implements RoboticArm {
     private static final int ARM_2_MIN = 1000;
     private static final int ARM_2_MAX = 1400;
     double arm1MinAngle, arm1MaxAngle,arm2MinAngle, arm2MaxAngle;
-   /*public static void main(String[] args) {
-        UI.initialise();
-
-        RoboticArmJNI arm = new RoboticArmJNI(287,374,377,374,154);
-        UI.addSlider("Servo 1",ARM_1_MIN,ARM_1_MAX,d->{arm.setServo(0,d);UI.println("Servo 1 pulse: "+d);});
-        UI.addSlider("Servo 2",ARM_2_MIN,ARM_2_MAX,d->{arm.setServo(1,d);UI.println("Servo 2 pulse: "+d);});
-        UI.addSlider("Servo 1 Angle",-180,0,d->arm.setAngle(d,arm.lastTheta2));
-        UI.addSlider("Servo 2 Angle",-180,0,d->arm.setAngle(arm.lastTheta1,d));
-        UI.addButton("Read Theta 1",()->UI.println("Theta 1: "+arm.readAngle(0)));
-        UI.addButton("Read Theta 2",()->UI.println("Theta 2: "+arm.readAngle(1)));
-        UI.addButton("Pen Down",()->arm.setPenMode(true));
-        UI.addButton("Pen Up",()->arm.setPenMode(false));
-        arm.init();
-        arm.calibrate();
-    }*/
     static {
         try {
             NativeLoader.loadLibrary("RoboticArrrmJNI-1.0-SNAPSHOT");
@@ -48,50 +33,53 @@ public class RoboticArmJNI implements RoboticArm {
         d=absLength(o1X,o2X,o1Y,o2Y);
         l=appendageLength;
         theModel = new RoboticArmModel(o1X,o1Y,o2X,o2Y,l);
-        init();
+
+        new Thread(this::init).run();
         calibrate();
     }
     public native void init();
     public native double readAngle(int servo);
     public native void setServo(int servo, double pulse);
     public void calibrate() {
-        setServo(0,ARM_1_MIN);
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        arm1MinAngle = readAngle(0);
-        setServo(0,ARM_1_MAX);
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        arm1MaxAngle = readAngle(0);
-        setServo(1,ARM_2_MIN);
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        arm2MinAngle = readAngle(1);
-        setServo(1,ARM_2_MAX);
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        arm2MaxAngle = readAngle(1);
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        mArm1 = ((ARM_1_MAX-ARM_1_MIN)/(arm1MaxAngle-arm1MinAngle));
-        mArm2 = ((ARM_2_MAX-ARM_2_MIN)/(arm2MaxAngle-arm2MinAngle));
-        cArm1 = ARM_1_MIN - mArm1*arm1MinAngle;
-        cArm2 = ARM_2_MIN - mArm2*arm2MinAngle;
+        new Thread(()-> {
+            setServo(0, ARM_1_MIN);
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            arm1MinAngle = readAngle(0);
+            setServo(0, ARM_1_MAX);
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            arm1MaxAngle = readAngle(0);
+            setServo(1, ARM_2_MIN);
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            arm2MinAngle = readAngle(1);
+            setServo(1, ARM_2_MAX);
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            arm2MaxAngle = readAngle(1);
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            mArm1 = ((ARM_1_MAX - ARM_1_MIN) / (arm1MaxAngle - arm1MinAngle));
+            mArm2 = ((ARM_2_MAX - ARM_2_MIN) / (arm2MaxAngle - arm2MinAngle));
+            cArm1 = ARM_1_MIN - mArm1 * arm1MinAngle;
+            cArm2 = ARM_2_MIN - mArm2 * arm2MinAngle;
+        }).start();
     }
     double lastTheta1 = -90;
     double lastTheta2 = -90;
