732497cd2380ed1472eaad5c7bda082e2e443c65
sanjay900 <sanjay.govind9@gmail.com>
2016-09-12 10:49:55 +1200

    Move website out of jar, so that maven doesnt try to shade it in. clean up JNI a little

--- RoboticArrrmJNI/src/main/java/net/tangentmc/RoboticArmJNI.java
+++ RoboticArrrmJNI/src/main/java/net/tangentmc/RoboticArmJNI.java
@@ -10,6 +10,8 @@ import java.io.PrintStream;
 import java.util.Arrays;
 
 public class RoboticArmJNI implements RoboticArm {
+    //These are calibration points, not real min/max.
+    //Thats handled by c.
     private static final int ARM_1_MIN = 1500;
     private static final int ARM_1_MAX = 2000;
     private static final int ARM_2_MIN = 1000;
@@ -22,31 +24,10 @@ public class RoboticArmJNI implements RoboticArm {
     private PrintStream out;
     private Process process;
 
-    public RoboticArmJNI(RoboticArmModel model) {
+    RoboticArmJNI(RoboticArmModel model) {
         theModel = model;
     }
 
-    private double readAngle(int servo) {
-        if (process == null) return -1;
-        out.println(MEASURE_ANGLE_COMMAND);
-        out.flush();
-        try {
-            String next;
-            while ((next = in.readLine())!= null) {
-                if (next.startsWith("measured")) {
-                    //The angles are separated by space
-                    String[] args = next.replace("measured angles: ","").split(" ");
-                    //The doubles are stored as theta=double so we want whats after the equals sign
-                    Trace.println(Arrays.toString(Arrays.stream(args).map(s2 -> s2.split("=")[1]).toArray()));
-                    return Double.parseDouble(args[servo].split("=")[1]);
-                }
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return -1;
-    }
     void setServo(int servo, int pulse) {
         if (process == null) return;
         out.println(SET_MOTOR_COMMAND);
@@ -70,34 +51,34 @@ public class RoboticArmJNI implements RoboticArm {
     private void calibrate() {
         Trace.println("Initialization:");
         //Wait for arm2 to start
-        UI.sleep(5000);
+        UI.sleep(2000);
         setPenMode(false);
-        //Read a few angles as the first few inputs get eaten by arm2
+        //Read a few angles as the first few inputs may get eaten by arm2
         readAngle(0);
         readAngle(0);
 
         Trace.println("Starting Calibration:");
         Trace.println("Arm 1 Min:");
         setServo(0, ARM_1_MIN);
-        UI.sleep(1000);
+        UI.sleep(2000);
         double arm1MinAngle = readAngle(0);
         UI.sleep(1000);
 
         Trace.println("Arm 1 Max:");
         setServo(0, ARM_1_MAX);
-        UI.sleep(1000);
+        UI.sleep(3000);
         double arm1MaxAngle = readAngle(0);
         UI.sleep(1000);
 
         Trace.println("Arm 2 Min:");
         setServo(1, ARM_2_MIN);
-        UI.sleep(1000);
+        UI.sleep(2000);
         double arm2MinAngle = readAngle(1);
         UI.sleep(1000);
 
         Trace.println("Arm 2 Max:");
         setServo(1, ARM_2_MAX);
-        UI.sleep(1000);
+        UI.sleep(2000);
         double arm2MaxAngle = readAngle(1);
         UI.sleep(1000);
 
@@ -106,10 +87,10 @@ public class RoboticArmJNI implements RoboticArm {
         mArm2 = ((ARM_2_MAX - ARM_2_MIN) / (arm2MaxAngle - arm2MinAngle));
         cArm1 = ARM_1_MIN - (mArm1 * arm1MinAngle);
         cArm2 = ARM_2_MIN - (mArm2 * arm2MinAngle);
-        System.out.println(mArm1);
-        System.out.println(mArm2);
-        System.out.println(cArm1);
-        System.out.println(cArm2);
+        Trace.println("mArm1: "+mArm1);
+        Trace.println("mArm2: "+mArm2);
+        Trace.println("cArm1: "+cArm1);
+        Trace.println("cArm2: "+cArm2);
     }
     @Override
     public void setAngle(double theta1, double theta2) {
@@ -121,10 +102,6 @@ public class RoboticArmJNI implements RoboticArm {
         setServo(0, pulse1);
         setServo(1, pulse2);
         UI.sleep(1);
-        Trace.println("Servo 1:"+theta1);
-        Trace.println("Servo 2:"+theta2);
-        Trace.println("Servo 1:"+pulse1);
-        Trace.println("Servo 2:"+pulse2);
     }
 
     @Override
@@ -138,9 +115,10 @@ public class RoboticArmJNI implements RoboticArm {
     }
 
     void init() throws Exception {
+        Trace.setVisible(true);
         //Script is required here, as there is a bug with java/c where the input stream
-        //is not correctly flushed otherwise, and we recieve no input.
-        //If a computer has the script command, and the script isnt found,
+        //is not correctly flushed otherwise, and we receive no input.
+        //If a computer has the script command, and the script inst found,
         //Or we forget sudo or the arm crashes, throw an exception
         ProcessBuilder builder = new ProcessBuilder("script","-c","/home/pi/Arm/arm2");
         process = builder.start();
@@ -149,11 +127,31 @@ public class RoboticArmJNI implements RoboticArm {
             process = null;
             throw new IOException("The arm failed to start!");
         }
-        Trace.setVisible(true);
         out = new PrintStream(process.getOutputStream());
-        in  = new BufferedReader(
-                new InputStreamReader((process.getInputStream())));
+        in  = new BufferedReader(new InputStreamReader((process.getInputStream())));
         calibrate();
+    }
+
+    private double readAngle(int servo) {
+        if (process == null) return -1;
+        out.println(MEASURE_ANGLE_COMMAND);
+        out.flush();
+        try {
+            String next;
+            while ((next = in.readLine())!= null) {
+                if (next.startsWith("measured")) {
+                    //The angles are separated by space
+                    String[] args = next.replace("measured angles: ","").split(" ");
+                    //The doubles are stored as theta=double so we want whats after the equals sign
+                    Trace.println(Arrays.toString(Arrays.stream(args).map(s2 -> s2.split("=")[1]).toArray()));
+                    return Double.parseDouble(args[servo].split("=")[1]);
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return -1;
     }
     private static final String SET_MOTOR_COMMAND = "w";
     private static final String MEASURE_ANGLE_COMMAND = "m";
