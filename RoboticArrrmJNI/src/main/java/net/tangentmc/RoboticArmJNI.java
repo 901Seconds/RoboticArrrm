8fdaeac75ffc61850f97530337e01e380db426f9
sanjay900 <sanjay.govind9@gmail.com>
2016-09-12 17:54:05 +1200

    Change picture

--- RoboticArrrmJNI/src/main/java/net/tangentmc/RoboticArmJNI.java
+++ RoboticArrrmJNI/src/main/java/net/tangentmc/RoboticArmJNI.java
@@ -114,7 +114,7 @@ class RoboticArmJNI implements RoboticArm {
     @Override
     public void setPenMode(boolean down) {
         setServo(2,down?2000:1000);
-        UI.sleep(150);
+        UI.sleep(200);
     }
 
     void init() throws Exception {
