bfc35fd4d82d9ec169f6ef8df8fd48670f187e72
sanjay900 <sanjay.govind9@gmail.com>
2016-09-26 16:45:07 +1300

    Focibly draw the point after interpolation

--- RoboticArrrmLauncher/src/main/java/net/tangentmc/Launcher.java
+++ RoboticArrrmLauncher/src/main/java/net/tangentmc/Launcher.java
@@ -29,7 +29,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Launcher {
     //Min distance between two points
-    static final double LINE_MIN_DIST =10;
+    static final double LINE_MIN_DIST =2;
     private BlockingQueue<DrawPoint> pointsToDraw = new LinkedBlockingQueue<>();
     private boolean left = false;
     private ArrayList<RoboticArm> arms = new ArrayList<>();
