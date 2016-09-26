b7eba047845a99fec96d05b9f2ed85d8c7b3bf97
sanjay900 <sanjay.govind9@gmail.com>
2016-09-26 15:57:03 +1300

    turn interp back on.

--- RoboticArrrmLauncher/src/main/java/net/tangentmc/Launcher.java
+++ RoboticArrrmLauncher/src/main/java/net/tangentmc/Launcher.java
@@ -29,7 +29,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Launcher {
     //Min distance between two points
-    static final double LINE_MIN_DIST = 100;
+    static final double LINE_MIN_DIST = 1;
     private BlockingQueue<DrawPoint> pointsToDraw = new LinkedBlockingQueue<>();
     private boolean left = false;
     private ArrayList<RoboticArm> arms = new ArrayList<>();
